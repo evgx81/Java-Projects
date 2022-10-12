8f05e82048109fc82135bae84ce967276a8aa78c
evgx81 <xoroshix2002@mail.ru>
2022-10-12 18:50:58 +0300

    added tests of class Node

---
+++ LinkedList/linked_list/src/test/java/evgx81/linked_list/NodeTest.java
@@ -0,0 +1,41 @@
+package evgx81.linked_list;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit тесты для класса Node.
+ */
+public class NodeTest {
+    @Test
+    public void createEmptyNode() {
+        Node<Integer> node = new Node<Integer>();
+
+        assertEquals(null, node.getPrevNode());
+        assertEquals(null, node.getNextNode());
+    }
+
+    @Test
+    public void createNotEmptyNode() {
+        Node<Integer> node = new Node<Integer>();
+        node.setData(5);
+
+        assertEquals(null, node.getPrevNode());
+        assertEquals(null, node.getNextNode());
+        assertEquals((Integer)5, node.getData());
+    }
+
+    @Test 
+    public void copyNode() {
+        Node<Integer> node1 = new Node<Integer>();
+        node1.setData(5);
+
+        Node<Integer> node2 = new Node<Integer>(node1);
+
+        assertEquals(null, node2.getPrevNode());
+        assertEquals(null, node2.getNextNode());
+        assertEquals((Integer)5, node2.getData());
+    }
+}
