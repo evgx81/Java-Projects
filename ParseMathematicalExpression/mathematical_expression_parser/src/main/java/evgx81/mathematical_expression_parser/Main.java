c0de4a6fab069dbb008127f02a02036c1315aa53
evgx81 <xoroshix2002@mail.ru>
2022-11-05 20:22:53 +0300

    some changes in Main class

--- ParseMathematicalExpression/mathematical_expression_parser/src/main/java/evgx81/mathematical_expression_parser/Main.java
+++ ParseMathematicalExpression/mathematical_expression_parser/src/main/java/evgx81/mathematical_expression_parser/Main.java
@@ -5,7 +5,8 @@ import java.util.HashMap;
 
 /**
  * Класс Main демонстрирует возможности класса Parser.
- *
+ * 
+ * @author evgx81 
  */
 public class Main 
 {
@@ -13,7 +14,7 @@ public class Main
     {   
         String exp1 = "abs(-3)+cos(0)^2+x*2";
         HashMap<String, Number> vars = new HashMap<String, Number>();
-        vars.put("x", 3);
+        vars.put("x", 5);
 
         Parser parser = new Parser(exp1, vars);
         BigDecimal res = parser.eval();
