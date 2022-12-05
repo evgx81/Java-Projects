d58936b4afdbee138706bc868b864aa25b33ae5b
evgx81 <xoroshix2002@mail.ru>
2022-12-05 12:04:56 +0300

    added class Division

---
+++ readFromCSV/readfromcsv/src/main/java/com/evgx81/Division.java
@@ -0,0 +1,51 @@
+package com.evgx81;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Класс Division содержит информацию оподразделении работника работнике.
+ * 
+ * @author evgx81
+ */
+public class Division {
+    transient private static AtomicInteger nextIDCounter = new AtomicInteger(1);
+
+    /**
+     *  Уникальный идентификатор подразделения.
+     */
+    private int id;
+
+    /**
+     *  Название подразделения.
+     */
+    private String name;
+
+    /**
+     * Конструктор для класса Division.
+     * 
+     * @param name название подразделения.
+     */
+    public Division(String name) {
+        this.id = nextIDCounter.getAndIncrement();
+        this.name = name;
+    }
+
+     /**
+     * Set и get методы для класса Division.
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
