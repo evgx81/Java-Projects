add4117ab656fad48847f5cf9213f12d3ec5755a
evgx81 <xoroshix2002@mail.ru>
2022-12-05 16:20:51 +0300

    added reading from file

--- readFromCSV/readfromcsv/src/main/java/com/evgx81/ReadFromCSV.java
+++ readFromCSV/readfromcsv/src/main/java/com/evgx81/ReadFromCSV.java
@@ -1,6 +1,7 @@
 package com.evgx81;
 
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.io.InputStream;
 
 import java.util.LinkedList;
@@ -8,6 +9,7 @@ import java.util.List;
 
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 import com.opencsv.CSVReader;
 
 /**
@@ -43,5 +45,18 @@ public class ReadFromCSV
 
         // Создаем пустой массив строк
         String[] nextLine;
+
+        // Считываем каждую строку в массив строк
+        try {
+            while ((nextLine = reader.readNext()) != null) {
+                Employee employee = new Employee(nextLine);
+                employees.add(employee);
+            }
+        } catch (IOException exception) {
+            System.out.println("Error while reading next line from file!");
+        } catch (CsvValidationException exception) {
+            System.out.println("Validation error in line " + 
+                exception.getLineNumber() + ": " + exception.getMessage());
+        }
     }
 }
