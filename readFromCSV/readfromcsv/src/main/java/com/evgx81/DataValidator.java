0900c348b816483afb4b16373067080e5945c603
evgx81 <xoroshix2002@mail.ru>
2022-12-06 11:54:33 +0300

    added override function isValid

--- readFromCSV/readfromcsv/src/main/java/com/evgx81/DataValidator.java
+++ readFromCSV/readfromcsv/src/main/java/com/evgx81/DataValidator.java
@@ -8,7 +8,7 @@ import com.opencsv.validators.RowValidator;
  * 
  * Класс DataValidator является наследником класса RawValidator, который имеет 
  * два обязательных метода: isValid и validate. 
- * s
+ * 
  * @author evgx81
  */
 public class DataValidator implements RowValidator {
@@ -16,4 +16,30 @@ public class DataValidator implements RowValidator {
      * Строка, которая содержит сообщение об ошибке. 
      */
     private String message;
+
+    /**
+     * 
+     */
+    @Override
+    public boolean isValid(String[] row) {
+        if (row.length != 6) {
+            message = "Row must contain only 6 columns";
+            return false;
+        }
+
+        try {
+            Integer.parseInt(row[0]);
+            Integer.parseInt(row[5]);
+        } catch (NumberFormatException ex) {
+            message = "ID and Salary must be integer";
+            return false;
+        }
+
+        if (row[2].equals("Female") && row[2].equals("Male")) {
+            message = "Gender must be Male or Female";
+            return false;
+        }
+
+        return true;
+    }
 }
