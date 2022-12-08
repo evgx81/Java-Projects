efb5e1289917d1f86b869c5fc2b910101c49d796
evgx81 <xoroshix2002@mail.ru>
2022-12-08 21:20:32 +0300

    add some changes

--- readFromCSV/readfromcsv/src/test/java/com/evgx81/EmployeeTest.java
+++ readFromCSV/readfromcsv/src/test/java/com/evgx81/EmployeeTest.java
@@ -1,8 +1,12 @@
 package com.evgx81;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
+
+
 
 /**
  * Unit тесты для класса Employee.
@@ -16,7 +20,7 @@ public class EmployeeTest {
         String[] test = {"1", "Employee1", "Female", "10.11.1978", "G", "1500"};
         Employee empl_test = new Employee(test);
 
-        assertEquals(empl_test.getID(), 1);
+        assertEquals("ID should be initialized", 1, empl_test.getID());
     }
 
     @Test
@@ -24,20 +28,28 @@ public class EmployeeTest {
         String[] test = {"1", "Employee1", "Female", "10.11.1978", "G", "1500"};
         Employee empl_test = new Employee(test);
 
-        assertEquals(empl_test.getName(), "Employee1");
+        assertEquals("Employee1", empl_test.getName());
+    }
+
+    @Test
+    public void testCreatedEmployeeGenderMale() {
+        String[] test = {"1", "Employee1", "Male", "10.11.1978", "G", "1500"};
+        Employee empl_test = new Employee(test);
+
+        assertEquals(Gender.MALE, empl_test.getGender());
     }
     
     @Test
-    public void testCreatedEmployeeGender() {
-        String[] test = {"1", "Emplyee1", "Female", "10.11.1978", "G", "1500"};
+    public void testCreatedEmployeeGenderFemale() {
+        String[] test = {"1", "Employee1", "Female", "10.11.1978", "G", "1500"};
         Employee empl_test = new Employee(test);
 
-        assertEquals(empl_test.getGender(), Gender.FEMALE);
+        assertEquals(Gender.FEMALE, empl_test.getGender());
     }
 
     @Test
     public void testCreatedEmployeeBirthDay() {
-        String[] test = {"1", "Emplyee1", "Female", "10.11.1978", "G", "1500"};
+        String[] test = {"1", "Employee1", "Female", "10.11.1978", "G", "1500"};
         Employee empl_test = new Employee(test);
 
         assertEquals(empl_test.getBirthDay(), "10.11.1978");
@@ -45,35 +57,39 @@ public class EmployeeTest {
 
     @Test
     public void testCreatedEmployeeDivisionName() {
-        String[] test = {"1", "Emplyee1", "Female", "10.11.1978", "G", "1500"};
+        String[] test = {"1", "Employee1", "Female", "10.11.1978", "G", "1500"};
         Employee empl_test = new Employee(test);
 
-        assertEquals(empl_test.getDivision().getName(), "G");
+        assertEquals("G", empl_test.getDivision().getName());
+    }
+
+    @Test
+    public void createdEmployeesWithDifferentDivisions() {
+        String[] test1 = {"1", "Employee1", "Female", "10.11.1978", "G", "1500"};
+        String[] test2 = {"2", "Employee2", "Female", "11.12.1878", "L", "15000"};
+
+        Employee empl_test1 = new Employee(test1);
+        Employee empl_test2 = new Employee(test2);
+
+        assertNotSame(empl_test1.getDivision(), empl_test2.getDivision());
+    }
+
+    @Test
+    public void createdEmployeesWithSameDivisions() {
+        String[] test1 = {"1", "Employee1", "Female", "10.11.1978", "L", "1500"};
+        String[] test2 = {"2", "Employee2", "Female", "11.12.1878", "L", "15000"};
+
+        Employee empl_test1 = new Employee(test1);
+        Employee empl_test2 = new Employee(test2);
+
+        assertSame(empl_test1.getDivision(), empl_test2.getDivision());
     }
 
     @Test
     public void testCreatedEmployeeSalary() {
-        String[] test = {"1", "Emplyee1", "Female", "10.11.1978", "G", "1500"};
+        String[] test = {"1", "Employee1", "Female", "10.11.1978", "G", "1500"};
         Employee empl_test = new Employee(test);
 
         assertEquals(empl_test.getSalary(), (Integer) 1500);
-    }
-
-    @Test(expected = NumberFormatException.class)
-    public void testCreatedEmployeeIDWithMistake() {
-        String[] test = {"1D", "Employee1", "Female", "10.11.1978", "G", "1500"};
-        Employee empl_test = new Employee(test);
-    }
-
-    @Test(expected = NumberFormatException.class)
-    public void testCreatedEmployeeGenderWithMistake() {
-        String[] test = {"1D", "Employee1", "Female1", "10.11.1978", "G", "1500"};
-        Employee empl_test = new Employee(test);
-    }
-
-    @Test(expected = NumberFormatException.class)
-    public void testCreatedEmployeeSalaryWithMistake() {
-        String[] test = {"1D", "Employee1", "Female1", "10.11.1978", "G", "15L00"};
-        Employee empl_test = new Employee(test);
     }
 }
