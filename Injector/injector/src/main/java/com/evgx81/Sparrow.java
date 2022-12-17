3a1b353a4a26685ce1e7e449dad0037fa940aac3
evgx81 <xoroshix2002@mail.ru>
2022-12-17 12:21:53 +0300

    added class Sparrow

---
+++ Injector/injector/src/main/java/com/evgx81/Sparrow.java
@@ -0,0 +1,15 @@
+package com.evgx81;
+
+/**
+ * Класс Sparrow реализует интерфейс Bird.
+ * 
+ * @author evgx81
+ */
+public class Sparrow implements Bird {
+
+    @Override
+    public void sing() {
+        System.out.println("Чик-чирик");
+    }
+    
+}
