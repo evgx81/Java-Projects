7e8cbf8938923d6fbedc7843a7bbc329074c16ca
evgx81 <xoroshix2002@mail.ru>
2022-12-17 12:22:51 +0300

    added class Rooster

---
+++ Injector/injector/src/main/java/com/evgx81/Rooster.java
@@ -0,0 +1,16 @@
+package com.evgx81;
+
+/**
+ * Класс Rooster реализует интерфейс Bird.
+ * 
+ * @author evgx81
+ */
+public class Rooster implements Bird {
+
+    @Override
+    public void sing() {
+        System.out.println("Ку-ка-ре-ку!");
+        
+    }
+    
+}
