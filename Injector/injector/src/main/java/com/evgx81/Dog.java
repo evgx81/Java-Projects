4be8f1721d487b808059dce7b277dbf3b0583dc5
evgx81 <xoroshix2002@mail.ru>
2022-12-19 21:05:00 +0300

    added class Dog

---
+++ Injector/injector/src/main/java/com/evgx81/Dog.java
@@ -0,0 +1,15 @@
+package com.evgx81;
+
+/**
+ * Класс Dog реализует интерфейс Mammal.
+ * 
+ * @author evgx81
+ */
+public class Dog implements Mammal {
+
+    @Override
+    public void say() {
+        System.out.println("Гав-гав!");
+        
+    }
+}
